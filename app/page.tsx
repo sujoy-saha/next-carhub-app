bb63a6fdb6d1d4b99d0f51abd2f1424e197caa44
sujoykumarsaha <sujoykumarsaha@gmail.com>
2023-07-19 22:47:19 +0200

    remove the display results for build error.

--- app/page.tsx
+++ app/page.tsx
@@ -37,9 +37,7 @@ export default async function Home({ searchParams }: HomeProps) {
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars?.map((car) => (
-                <CarCard car={car} />
-              ))}
+              
             </div>
             <ShowMore
               pageNumber={(searchParams.limit || 10) / 10}
